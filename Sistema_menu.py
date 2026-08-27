21338e39c8a72b545d8ddd8a16ab9f00f5d69d14
Carlos Dev <flaviocarlosdejesusouza@gmail.com>
2026-08-27 15:38:25 -0300

    Implement main menu loop in Sistema_menu.py

---
+++ Sistema_menu.py
@@ -0,0 +1,33 @@
+
+while True :
+    print("")
+    print("")
+    print("   [ Menu ]   ")
+    print("")
+    print("1. Teste")
+    print("2. Teste")
+    print("3. Teste")
+    print("4. Sair do Sistema")
+    print("")
+    
+    i = input("Opção: ")
+    
+    if i == "1":
+        print("")
+        print("Informações da opção 1")
+        
+    elif i == "2":
+        print("")
+        print("informações da opção 2")
+        
+    elif i == "3":
+        print("")
+        print("informações da opção 3")
+        
+    elif i == "4":
+        print("")
+        print("Saindo...")
+        break
+        
+    else:
+        print("Opção Desconhecida")
